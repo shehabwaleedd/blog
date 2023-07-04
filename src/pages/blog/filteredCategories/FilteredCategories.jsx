6f4111b3517649d16ce335dd53a621b35995905b
Shehab Waleed <shehabwaleedd@gmail.com>
2023-07-05 01:15:17 +0400

    build version

--- src/pages/blog/filteredCategories/FilteredCategories.jsx
+++ src/pages/blog/filteredCategories/FilteredCategories.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import Loading from "../../loading/Loading.tsx";
 
-const FilteredCategories = ({navOpen, toggleTheme}) => {
+const FilteredCategories = ({ navOpen, toggleTheme }) => {
     const { category } = useParams();
     const navigate = useNavigate();
     const [posts, setPosts] = useState([]);
@@ -51,54 +51,49 @@ const FilteredCategories = ({navOpen, toggleTheme}) => {
 
 
     return (
-        <section className="filtered section">
-            <FilteredCategoriesTabs
-                categories={categories}
-                selectedCategory={selectedCategory}
-                onCategoryClick={handleCategoryClick}
-                navOpen={navOpen}
-                toggleTheme={toggleTheme}
-            />
-
-            <div className="filtered__container grid3 container">
-                {filteredPosts.map((post) => {
-                    return (
-                        <motion.div layout animate={{ opacity: 1, scale: 1 }} initial={{ opacity: 0, scale: 1 }} exit={{ opacity: 0, scale: 1 }} transition={{ duration: 0.5 }} className="post__box post__boxItems" key={post.id}>
-                            <AnimatePresence>
-                                <div className="blog__card_details">
-                                    <Link to={`/details/${post.id}`} className="link">
-                                        <div className="post__img-filtered">
-                                            <img src={post.imageUrls} alt="" />
+        <>
+            <FilteredCategoriesTabs navOpen={navOpen} toggleTheme={toggleTheme} categories={categories} selectedCategory={selectedCategory} onCategoryClick={handleCategoryClick} />
+            <section className="filtered section">
+                <div className="filtered__container grid3 container">
+                    {filteredPosts.map((post) => {
+                        return (
+                            <motion.div layout animate={{ opacity: 1, scale: 1 }} initial={{ opacity: 0, scale: 1 }} exit={{ opacity: 0, scale: 1 }} transition={{ duration: 0.5 }} className="post__box post__boxItems" key={post.id}>
+                                <AnimatePresence>
+                                    <div className="blog__card_details">
+                                        <Link to={`/details/${post.id}`} className="link">
+                                            <div className="post__img-filtered">
+                                                <img src={post.imageUrls} alt="" />
+                                            </div>
+                                            <div className="post__tag-filtered">
+                                                <a
+                                                    href="/"
+                                                    className="post__hashtag-filtered"
+                                                    onClick={(e) => {
+                                                        e.preventDefault();
+                                                        handleCategoryClick(post.category);
+                                                    }}
+                                                >
+                                                    #{post.category}
+                                                </a>
+                                            </div>
+                                            <h3 className="post__title-name-filtered">{post.title.slice(0,30)}...</h3>
+                                        </Link>
+                                        <p className="post__subtitle-filtered">{post.postText.slice(0, 90)}...</p>
+                                        <div className="post__date-filtered">
+                                            <div className="post__date-filtered-imgname">
+                                                <img src={post.photoURL} alt="" />
+                                                <span className="blog__post-author-filtered">@{post.author.name}</span>
+                                            </div>
+                                            <label htmlFor="">{post.date}</label>
                                         </div>
-                                        <div className="post__tag-filtered">
-                                            <a
-                                                href="/"
-                                                className="post__hashtag-filtered"
-                                                onClick={(e) => {
-                                                    e.preventDefault();
-                                                    handleCategoryClick(post.category);
-                                                }}
-                                            >
-                                                #{post.category}
-                                            </a>
-                                        </div>
-                                        <h3 className="post__title-name-filtered">{post.title}</h3>
-                                    </Link>
-                                    <p className="post__subtitle-filtered">{post.postText.slice(0, 90)}...</p>
-                                    <div className="post__date-filtered">
-                                        <div className="post__date-filtered-imgname">
-                                            <img src={post.photoURL} alt="" />
-                                            <span className="blog__post-author-filtered">@{post.author.name}</span>
-                                        </div>
-                                        <label htmlFor="">{post.date}</label>
                                     </div>
-                                </div>
-                            </AnimatePresence>
-                        </motion.div>
-                    );
-                })}
-            </div>
-        </section>
+                                </AnimatePresence>
+                            </motion.div>
+                        );
+                    })}
+                </div>
+            </section>
+        </>
     );
 };
 
